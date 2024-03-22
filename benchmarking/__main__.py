5f58764654aae07d99ff7c7f06ea7901321b974e
Billy Snikkers <billyprice98is@gmail.com>
2024-03-22 14:59:23 +0100

    faster qubo generation

--- benchmarking/__main__.py
+++ benchmarking/__main__.py
@@ -1,8 +1,8 @@
 from benchmarking import RandomParams, Benchmark, new_random_benchmark, Solver, new_graph_file_benchmark
 import numpy as np
 
-# params_code = "[RandomParams(directed=True, num_vertices=50, average_degree=a) for a in np.arange(1, 8.5, 0.5) for _ in range(3)]"
-# params_list = eval(params_code)
+params_code = "[RandomParams(directed=True, num_vertices=50, average_degree=a) for a in np.arange(1, 8.5, 0.5) for _ in range(3)]"
+params_list = eval(params_code)
 
 benchmark = new_graph_file_benchmark(solvers=[
 	Solver("brute", "FAST_BOUND"),
