6a3a961097194703158b09759941250b6a4772a8
robsorb <rob.schellingerhout@xs4all.nl>
2024-03-22 12:21:07 +0100

    automatically add kalp_files

--- benchmarking/__main__.py
+++ benchmarking/__main__.py
@@ -1,14 +1,16 @@
 from benchmarking import RandomParams, Benchmark, new_random_benchmark, Solver, new_graph_file_benchmark
 import numpy as np
 
-# params_code = "[RandomParams(directed=True, num_vertices=50, average_degree=a) for a in np.arange(1, 8.5, 0.5) for _ in range(3)]"
-# params_list = eval(params_code)
+params_code = "[RandomParams(directed=True, num_vertices=50, average_degree=a) for a in np.arange(1, 8.5, 0.5) for _ in range(3)]"
+params_list = eval(params_code)
 
-benchmark = new_graph_file_benchmark(solvers=[
-	Solver("brute", "FAST_BOUND"),
-	Solver("brute", "BRUTE_FORCE"),
+benchmark = new_random_benchmark(params_list, solvers=[
+	# Solver("brute", "FAST_BOUND"),
+	# Solver("brute", "BRUTE_FORCE"),
+	Solver("kalp", threads=8),
 	# Solver("ilp"),
-], graph_path="datasets/citation/citation-graph.txt", benchmark_path="benchmarks/citation")
+])
+# , graph_path="datasets/citation/citation-graph.txt", benchmark_path="benchmarks/citation")
 
 # benchmark = Benchmark.load_latest()
 
